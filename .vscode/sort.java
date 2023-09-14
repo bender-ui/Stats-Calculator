ea4017f3a5626e5de32ab75593a4f45b30d205a5
bender-ui <lm063875@gmail.com>
2023-09-14 19:39:29 -0400

    msg

---
+++ .vscode/sort.java
@@ -0,0 +1,15 @@
+package.vscode;
+
+public class sort {
+    public static void main(String args[]){
+       System.out.println(args);
+    }
+
+    public String[] sortString(String inputStr){
+        
+        
+        String[] parts = inputStr.split(", ");
+        return parts;
+
+    }
+}
