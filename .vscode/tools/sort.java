0fc19711fe31a48d3bb6a536cc4818bc7e1acdf3
Jacob <jacob.chiu168@hotmail.com>
2023-09-14 23:28:09 -0400

    handle bad input

---
+++ .vscode/tools/sort.java
@@ -0,0 +1,17 @@
+public class sort {
+    public static void main(String args[]){
+       System.out.println(args);
+    }
+
+    public String[] sortString(String inputStr){
+        
+        try{
+            String[] parts = inputStr.split(", ");
+            return parts;
+        } catch (NumberFormatException e) {
+            System.err.println("Bad input: " + e.getMessage());
+            String[] nun = new String['0'];
+            return nun;
+        }
+    }
+}
